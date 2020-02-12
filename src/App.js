7a8a45145553dd63d28d91bbd6d3281e8ef754df
idoia <ipalomabel1@educacion.navarra.es>
2020-02-12 14:06:43 +0100

    hasta el video 13

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,9 +7,23 @@ import './App.css';
 //}
 
 //const Hello = (props) => <h2>{props.titulo}</h2>
-class Hello extends Component{
-  render(){
+class Hello extends Component {
+  render() {
     return <h2>{this.props.titulo}</h2>
+  }
+}
+
+class Text extends Component {
+  render() {
+    const textoSegunBool = this.props.boolean ? 'On' : 'Off'
+    const mappedNumbers = this.props.arrayOfNumbers.map(n => n * 2)
+    return <div>
+      <p>{this.props.text}</p>
+      <p>{this.props.number}</p>
+      <p>{textoSegunBool}</p>
+      <p>{mappedNumbers.join(', ')}</p>
+      <p>{this.props.objectWithInfo.key}</p>
+    </div>
   }
 }
 
@@ -18,7 +32,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello titulo="Hello from props"/>
+        <Hello titulo="Hello from props" />
+        <Text
+          arrayOfNumbers={[2,3,10]}
+          objectWithInfo={{key:'First value', key2: 'otherValue'}}
+          number={2}
+          text="Texto en string"
+          isActivate 
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
